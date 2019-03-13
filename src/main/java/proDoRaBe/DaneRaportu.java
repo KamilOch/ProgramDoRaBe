ac6a5e2e2f17834d29186659ce808c93346177cd
Kamil Ochnik <ilmak@o2.pl>
2019-03-13 15:27:01 +0100

    dodalem sprawdzanie wprowadzanych danych

--- src/main/java/proDoRaBe/DaneRaportu.java
+++ src/main/java/proDoRaBe/DaneRaportu.java
@@ -13,16 +13,43 @@ public class DaneRaportu {
     private String adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego;
 
     public void dodajDaneRaportu (String nazwaFirmy, String adresFirmy, String nazwaLinii, String autorRaportu, String telefonKontaktowyDoAutoraRaportu, String adresEmailDoAutoraRaportu, String osobaOdpowiedzialnaPoStronieZamawiajacejRaport, String telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego, String adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego){
-        this.nazwaFirmy = nazwaFirmy;
-        this.adresFirmy = adresFirmy;
-        this.nazwaLinii = nazwaLinii;
-        this.autorRaportu = autorRaportu;
-        this.telefonKontaktowyDoAutoraRaportu = telefonKontaktowyDoAutoraRaportu;
-        this.adresEmailDoAutoraRaportu = adresEmailDoAutoraRaportu;
-        this.osobaOdpowiedzialnaPoStronieZamawiajacejRaport = osobaOdpowiedzialnaPoStronieZamawiajacejRaport;
-        this.telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego = telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego;
-        this.adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego = adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego;
+
+        if (czyPodanoWartosc(nazwaFirmy)){
+            this.nazwaFirmy = nazwaFirmy;
+        }
+        if (czyPodanoWartosc(adresFirmy)){
+            this.adresFirmy = adresFirmy;
+        }
+        if(czyPodanoWartosc(nazwaLinii)) {
+            this.nazwaLinii = nazwaLinii;
+        }
+        if(czyPodanoWartosc(autorRaportu)) {
+            this.autorRaportu = autorRaportu;
+        }
+        if(czyPodanoWartosc(telefonKontaktowyDoAutoraRaportu)) {
+            this.telefonKontaktowyDoAutoraRaportu = telefonKontaktowyDoAutoraRaportu;
+        }
+        if(czyPodanoWartosc(adresEmailDoAutoraRaportu)) {
+            this.adresEmailDoAutoraRaportu = adresEmailDoAutoraRaportu;
+        }
+        if(czyPodanoWartosc(osobaOdpowiedzialnaPoStronieZamawiajacejRaport)) {
+            this.osobaOdpowiedzialnaPoStronieZamawiajacejRaport = osobaOdpowiedzialnaPoStronieZamawiajacejRaport;
+        }
+        if(czyPodanoWartosc(telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego)) {
+            this.telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego = telefonKontaktowyDoOsobyOsobyOdpowiedzialnejPoStronieZamawiajacego;
+        }
+        if(czyPodanoWartosc(adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego)) {
+            this.adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego = adresEmailDoOsobyOdpowiedzialnejPoStronieZamawiajacego;
+        }
     }
+
+    private boolean czyPodanoWartosc (String watsocSprawdzana){
+       if (watsocSprawdzana==null || watsocSprawdzana.isEmpty()){
+           return false;
+       } else return true;
+
+    }
+
 
 
     public String getNazwaFirmy() {
