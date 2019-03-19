0b20060fc9c741f8cbd64c52b6ea025e4e945f50
Kamil Ochnik <ilmak@o2.pl>
2019-03-19 14:20:10 +0100

    dodaje elementy do list w konstruktorze

--- src/main/java/proDoRaBe/db/ReferencjeElementyCiala.java
+++ src/main/java/proDoRaBe/db/ReferencjeElementyCiala.java
@@ -2,9 +2,11 @@ package proDoRaBe.db;
 
 public class ReferencjeElementyCiala {
 
+    public ReferencjeElementyCiala() {
         elementyCiala.add("Kończyny górne");
         elementyCiala.add("Kończyny dolne");
         elementyCiala.add("Całe ciało");
         elementyCiala.add("Głowa");
         elementyCiala.add("Dłoń/Palec");
+    }
 }
--- src/main/java/proDoRaBe/db/ReferencjeStopienMozliwychObrazen.java
+++ src/main/java/proDoRaBe/db/ReferencjeStopienMozliwychObrazen.java
@@ -6,17 +6,16 @@ import java.util.HashMap;
 
 public class ReferencjeStopienMozliwychObrazen {
 
-        listaMozliwychObrazen.put("Zadrapanie / Stłuczenie", 0.25);
-        listaMozliwychObrazen.put("Skaleczenie / łagodny skutek", 0.5);
-        listaMozliwychObrazen.put("Złamanie drobne – palca od ręki lub nogi", 30.0);
-        listaMozliwychObrazen.put("Złamanie ręki, ramienia lub nogi", 5.0);
-        listaMozliwychObrazen.put("Utrata 1 lub kilku palców", 8.0);
-        listaMozliwychObrazen.put("Amputacja ręki lub nogi, częściowa utrata wzroku lub słuchu", 11.0);
-        listaMozliwychObrazen.put("Amputacja obu rąk, całkowita utrata wzroku lub słuchu", 15.0);
-        listaMozliwychObrazen.put("Zagrożenie życia lub trwała utrata zdrowia", 25.0);
-        listaMozliwychObrazen.put("Fatalne – pojedyńcza ofiara śmiertelna", 40.0);
-        listaMozliwychObrazen.put("Katastrofa – kilka ofiar śmiertelnych", 65.0);
-
-
-
+        public ReferencjeStopienMozliwychObrazen() {
+                listaMozliwychObrazen.put("Zadrapanie / Stłuczenie", 0.25);
+                listaMozliwychObrazen.put("Skaleczenie / łagodny skutek", 0.5);
+                listaMozliwychObrazen.put("Złamanie drobne – palca od ręki lub nogi", 30.0);
+                listaMozliwychObrazen.put("Złamanie ręki, ramienia lub nogi", 5.0);
+                listaMozliwychObrazen.put("Utrata 1 lub kilku palców", 8.0);
+                listaMozliwychObrazen.put("Amputacja ręki lub nogi, częściowa utrata wzroku lub słuchu", 11.0);
+                listaMozliwychObrazen.put("Amputacja obu rąk, całkowita utrata wzroku lub słuchu", 15.0);
+                listaMozliwychObrazen.put("Zagrożenie życia lub trwała utrata zdrowia", 25.0);
+                listaMozliwychObrazen.put("Fatalne – pojedyńcza ofiara śmiertelna", 40.0);
+                listaMozliwychObrazen.put("Katastrofa – kilka ofiar śmiertelnych", 65.0);
+        }
 }
