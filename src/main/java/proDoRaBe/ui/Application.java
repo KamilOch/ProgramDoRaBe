0707d861476e6fb0c4cfa52690798623edb81ff7
Piotr Przybylak <piotr.przybylak@tomtom.com>
2019-06-16 18:34:31 +0200

    Log db url

--- src/main/java/proDoRaBe/ui/Application.java
+++ src/main/java/proDoRaBe/ui/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("JDBC_DATABASE_URL: " + System.getenv("JDBC_DATABASE_URL"));
         SpringApplication.run(Application.class, args);
     }
 }
